e5bab41762b2c30cea6c5e108c2a844d8b8a28f0
Eren Düzova <erenduzova@gmail.com>
2022-12-25 11:17:39 +0300

    Same answers, answers gathered to single file Removed answer for question 3 ( wrong answer )

--- homework-answers/emlakcepte/src/main/java/com/eren/emlakcepte/service/BannerService.java
+++ homework-answers/emlakcepte/src/main/java/com/eren/emlakcepte/service/BannerService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,29 +18,12 @@ public class BannerService {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    final String ROOT_URI = "https://localhost:8080/banners";
-
-    public List<Banner> getAllBanner() {
-        ResponseEntity<Banner[]> response = restTemplate.getForEntity(ROOT_URI, Banner[].class);
-        return Arrays.asList(response.getBody());
-
-    }
-
-    public HttpStatus addBanner(Banner banner) {
-        ResponseEntity<HttpStatus> response = restTemplate.postForEntity(ROOT_URI, banner, HttpStatus.class);
-        return response.getBody();
-    }
-
     public Banner firstBanner(Realty realty) {
         return new Banner(String.valueOf(realty.getNo()), 1,
                 realty.getUser().getPhone(), "",
                 realty.getProvince() + realty.getDistrict());
     }
 
-    /*
     public void create(Banner banner) {
         bannerRepository.save(banner);
         System.out.println("BannerService :: banner kaydedildi");
@@ -51,6 +33,6 @@ public class BannerService {
     public List<Banner> getAll() {
         return bannerRepository.findAll();
     }
-*/
+
 
 }
